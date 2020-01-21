d9599db4269f98760ff8d08c7256fb79920be846
Nevets08 <Nevets08@users.noreply.github.com>
2020-01-21 13:16:48 +0100

    MAJ 21/01/2020

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const content = document.querySelector('#menu');
+const insertTarget = document.querySelector('#Apropods');
+scrollnav.init(content, { 
+  debug: false,
+  insertTarget: insertTarget,
+  insertLocation: 'append'
+});
